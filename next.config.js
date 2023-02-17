da7af40f131ca880ac562d13bdd1521c8150640a
zerogoon <kckzero@gmail.com>
2023-02-17 23:35:01 +0900

    #NextJS Intro Completed

--- next.config.js
+++ next.config.js
@@ -1,6 +1,30 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+const API_KEY = process.env.API_KEY;
+
+const nextConfig = {
+    reactStrictMode: true,
+    async redirects() {
+        return [
+            {
+                source: "/contact",
+                destination: "/form",
+                permanent: false,
+            },
+        ];
+    },
+    async rewrites() {
+        return [
+            {
+                source: "/api/movies",
+                destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+            },
+            {
+                source: "/api/movies/id",
+                destination: `https://api.themoviedb.org/3/movie/:id?api_key=${API_KEY}`,
+            },
+        ];
+    },
+};
+
+module.exports = nextConfig;
--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,56 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
-const API_KEY = "10923b261ba94d897ac6b81148314a3f";
-
-export default function Home() {
-    const [movies, setMovies] = useState();
-    useEffect(() => {
-        (async () => {
-            const { results } = await (await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`)).json();
-            setMovies(results);
-        })();
-    }, []);
+export default function Home({ results }) {
+    const router = useRouter();
+    const onClick = (id, title) => {
+        router.push(`/movies/${title}/${id}`);
+    };
     return (
-        <div>
+        <div className="container">
             <Seo title="Home" />
-            {!movies && <h4>Loading...</h4>}
-            {movies?.map((movie) => (
-                <div key={movie.id}>
-                    <h4>{movie.original_title}</h4>
+            {results?.map((movie) => (
+                <div onClick={() => onClick(movie.id, movie.original_title)} className="movie" key={movie.id}>
+                    <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+                    <h4>
+                        <Link href={`/movies/${movie.original_title}/${movie.id}`} legacyBehavior>
+                            <a>{movie.original_title}</a>
+                        </Link>
+                    </h4>
                 </div>
             ))}
+            <style jsx>{`
+                .container {
+                    display: grid;
+                    grid-template-columns: 1fr 1fr;
+                    padding: 20px;
+                    gap: 20px;
+                }
+                .movie img {
+                    max-width: 100%;
+                    border-radius: 12px;
+                    transition: transform 0.2s ease-in-out;
+                    box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+                }
+                .movie:hover img {
+                    transform: scale(1.05) translateY(-10px);
+                }
+                .movie h4 {
+                    font-size: 18px;
+                    text-align: center;
+                }
+            `}</style>
         </div>
     );
+}
+
+export async function getServerSideProps() {
+    const { results } = await (await fetch(`http://localhost:3000/api/movies`)).json();
+    return {
+        props: {
+            results,
+        },
+    };
 }
